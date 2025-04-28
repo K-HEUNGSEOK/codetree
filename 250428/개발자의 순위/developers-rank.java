6c6ee561576a4d0a012f8c539c36ca5c4f312da0
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-04-28 21:15:26 +0900

    Title: 개발자의 순위, Time: 151ms, Memory: 10MB, Status: Passed - Codetree

--- 250428/개발자의 순위/developers-rank.java
+++ 250428/개발자의 순위/developers-rank.java
@@ -6,61 +6,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-    
+       
         Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt(); // 반복횟수
-        int n = sc.nextInt(); // 사람 명수
-        Human[][] humans = new Human[k][n];
-
-        for(int i = 0 ; i < k; i ++){
-            for(int j = 0 ; j < n; j ++){
-                humans[i][j] = new Human(j+1, sc.nextInt());
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int [][] arr = new int[11][21];
+        int [][] rank = new int[11][n+1];
+        for(int i = 0; i < k ; i ++){
+            for(int j = 0 ; j < n ; j ++){
+                arr[i][j] = sc.nextInt();
+                rank[i][arr[i][j]] = j;
             }
         }
-        int[][] rank = new int[11][21];
-
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < n; j++) {
-                int personNum = humans[i][j].num;
-                rank[i][personNum] = j;
-            }
-        }
-
-        int ans = 0;
+        int cnt = 0;
         for(int a = 1; a <= n ; a ++){
-            for(int b = 1; b<=n ; b++){
-                if (a == b) continue;
-                boolean flag = true;
-                for(int round = 0; round < k ; round ++){
-                    if (rank[round][a] >= rank[round][b]){
-                        flag = false;
+            for(int b = 1; b <=n ;b ++){
+                if (a== b) continue;
+
+                boolean check =true;
+
+                for(int x = 0 ; x < k; x ++){
+                    if (rank[x][a] > rank[x][b]){
+                        check= false;
                         break;
                     }
                 }
-                if (flag){
-                    ans++;
+                if (check){
+                    cnt ++;
                 }
             }
         }
-        System.out.println(ans);
-
+        System.out.println(cnt);
     }
-    static class Human{
-        int index;
-        int num;
-
-        public Human(int index, int num) {
-            this.index = index;
-            this.num = num;
-        }
-
-        @Override
-        public String toString() {
-            return "Human{" +
-                    "index=" + index +
-                    ", num=" + num +
-                    '}';
-        }
-    }
-
 }
