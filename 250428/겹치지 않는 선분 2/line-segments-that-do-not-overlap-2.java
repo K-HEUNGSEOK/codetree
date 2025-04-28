dc818d68cb71f19da774e30e01305e1c9050f413
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-04-28 09:26:48 +0900

    Title: 겹치지 않는 선분 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250428/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
+++ 250428/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -1,52 +1,36 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-       static int MAX_VALUE = 1000000;
-    public static void main(String[] args) throws IOException {
-    
+    public static void main(String[] args) throws FileNotFoundException {
+ 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] x = new int[101];
-        int[] y = new int[101];
-        int offset = MAX_VALUE;
-        for(int i = 0 ; i < n ; i++){
-            x[i] = sc.nextInt() +offset;
-            y[i] = sc.nextInt() + offset;
+        int[][] seg = new int[n][2];
+        // 1) 입력받아서 [작은값, 큰값] 형태로 저장
+        for(int i = 0; i < n; i++){
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            seg[i][0] = Math.min(x1, x2);
+            seg[i][1] = Math.max(x1, x2);
         }
-        int count =  0;
-        for(int i = 0 ; i <n ; i ++){
-            boolean[] countingBox = new boolean[(MAX_VALUE * 2) + 1];
-            boolean check = true;
-            int ix = x[i];
-            int iy = y[i];
-            for(int k = ix; k <= iy; k++){
-                countingBox[k] = true;
-            }
-            for(int j = i+1 ; j < n ; j ++){
 
-                int jx = x[j], jy = y[j];
-
-
-                for(int k = jx; k <= jy; k++){
-                    countingBox[k] = false;
-                }
-            }
-
-            for(int j = ix; j <=iy; j ++){
-                if (!countingBox[j]){
-                    check =false;
+        int count = 0;
+        // 2) i번째 선분이 다른 어떤 선분과도 cross 되지 않으면 count++
+        for(int i = 0; i < n; i++){
+            boolean isolated = true;
+            for(int j = 0; j < n; j++){
+                if(i == j) continue;
+                // (seg[i].a - seg[j].a)*(seg[i].b - seg[j].b) < 0 이면 두 선분이 뒤집혀서 교차(cross) 하는 경우
+                if((seg[i][0] - seg[j][0]) * (seg[i][1] - seg[j][1]) < 0) {
+                    isolated = false;
                     break;
                 }
             }
-            if (check){
-                count++;
-            }
+            if(isolated) count++;
         }
+
         System.out.println(count);
     }
 }
