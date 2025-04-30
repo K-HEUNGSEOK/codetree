c6f45899ae002f88ec96c25b55f0af210be97d03
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-04-30 15:23:47 +0900

    Title: 스승의 은혜 3, Time: 249ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250430/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 250430/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -6,50 +6,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-     
+      
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // 사람 수
         int b = sc.nextInt(); // 선물 살 수 있는 예산
         int ans = 0; //정답
-        Student[] students = new Student[1001];
+        int [] p = new int[1001];
+        int [] s = new int[1001];
 
         for(int i = 0 ; i < n ; i ++){
-            students[i] = new Student(sc.nextInt(),sc.nextInt());
+            p[i] = sc.nextInt();
+            s[i]= sc.nextInt();
         }
 
         for(int i = 0 ; i < n ; i ++){
-            Student[] temp = new Student[n];
-            for(int j = 0 ; j < n ; j ++){
-                temp[j] = new Student(students[j].price,students[j].delivery);
+            int[] temp = new int[1001];
+            for(int k = 0; k < n ; k ++){
+                temp[k] = p[k] + s[k];
             }
-            temp[i].price /= 2;
             Arrays.sort(temp,0,n);
-
-            int sum = 0 ;
-            int count = 0 ;
+            temp[i] /= 2;
+            int count = 0;
+            int price = temp[i];
             for(int j = 0 ; j < n ; j ++){
-                sum += temp[j].delivery + temp[j].price;
-                if (sum > b){
+                if (price > b){
                     break;
                 }
-                count++;
+                count ++;
+                price += temp[j];
             }
             ans = Math.max(ans,count);
         }
         System.out.println(ans);
-
-    }
-    static class Student implements Comparable<Student>{
-        int price;
-        int delivery;
-
-        public Student(int price, int delivery) {
-            this.price = price;
-            this.delivery = delivery;
-        }
-        @Override
-        public int compareTo(Student o){
-            return (this.price+this.delivery) - (o.price+o.delivery);
-        }
     }
 }
