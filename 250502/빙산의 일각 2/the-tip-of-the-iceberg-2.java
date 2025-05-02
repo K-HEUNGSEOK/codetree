11b7d4be57a44b371f61b9c0f395909aacd76dd3
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-02 14:06:23 +0900

    Title: 빙산의 일각 2, Time: 150ms, Memory: 11MB, Status: Passed - Codetree

--- 250502/빙산의 일각 2/the-tip-of-the-iceberg-2.java
+++ 250502/빙산의 일각 2/the-tip-of-the-iceberg-2.java
@@ -6,7 +6,7 @@
 
     public class Main {
         public static void main(String[] args) throws FileNotFoundException {
-       
+          
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
             int [] arr = new int[n];
@@ -16,28 +16,26 @@
             }
 
             int ans = 0;
-            for(int h = 1; h < 1000; h ++){
-                boolean[] check = new boolean[n];
-                for(int i = 0 ; i < n ; i ++){
-                    int value = arr[i] - h;
-                    if (value <= 0){
-                        continue;
-                    }
-                    check[i] = true;
-                }
+
+            for(int h = 1 ; h < 1000; h ++){
+
+                boolean [] check = new boolean[n];
                 int cnt = 0;
-                for(int j = 0 ; j < n ; j ++){
-                    if (check[j]){
-                       int l = j + 1;
-                       while (l < n && check[l]){
-                           l ++;
-                       }
-                       cnt ++;
-                       j = l;
+                for(int i = 0 ; i < n ; i ++){
+                    if (arr[i] - h > 0){
+                        check[i] = true;
+                    }
+                }
+                if (check[0]){
+                    cnt++;
+                }
+
+                for(int j = 1 ; j < n ; j ++){
+                    if (check[j] && !check[j-1]){
+                        cnt ++;
                     }
                 }
                 ans = Math.max(ans,cnt);
-
             }
             System.out.println(ans);
     }
