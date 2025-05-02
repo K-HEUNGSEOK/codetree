1283a58f3a4b5a2b129f6a05f8fe214c5735c056
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-02 14:31:58 +0900

    Title: 팰린드롬 수 찾기, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

--- 250502/팰린드롬 수 찾기/find-the-number-of-palindrome.java
+++ 250502/팰린드롬 수 찾기/find-the-number-of-palindrome.java
@@ -6,7 +6,7 @@
 
     public class Main {
         public static void main(String[] args) throws FileNotFoundException {
-          
+           
             Scanner sc = new Scanner(System.in);
             int cnt = 0;
             int x = sc.nextInt();
@@ -19,23 +19,12 @@
             System.out.println(cnt);
     }
     static int binary(int i){
-            int [] arr = new int[21];
-            int index = 0;
+            int original = i;
+            int result = 0;
             while (i > 0){
-                arr[index ++] = i % 10;
+                result = result * 10 + (i % 10);
                 i /= 10;
             }
-            for(int j = index-1 ; j > 0; j -- ){
-                int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
-            }
-
-            StringBuilder sb = new StringBuilder();
-        for(int j = 0; j < index; j ++){
-            sb.append(arr[j]);
-        }
-        Integer result = Integer.valueOf(sb.toString());
-        return result;
+            return result;
     }
     }
