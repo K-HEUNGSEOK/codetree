ebafc95207eef1678b75e7ef7c22d728df7b85f2
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-05 00:02:30 +0900

    Title: 팀으로 하는 틱택토 2, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250505/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -0,0 +1,132 @@
+    import javax.swing.plaf.PanelUI;
+    import java.io.FileInputStream;
+    import java.io.FileNotFoundException;
+    import java.util.Arrays;
+    import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) throws FileNotFoundException {
+           
+            Scanner sc = new Scanner(System.in);
+            char[][] arr = new char[3][3];
+            for(int i = 0 ; i < 3; i ++){
+                arr[i] = sc.next().toCharArray();
+            }
+            int ans = 0;
+            for(int i = 1; i <= 9; i ++){
+                for(int j = i+1; j <= 9 ; j ++){
+                        if (i == j) continue;
+                        int count = 0;
+                    if (first(arr,i,j,count) ||
+                            second(arr,i,j,count)||
+                            width(arr,i,j,count)||
+                            height(arr,i,j,count)){
+                        ans ++;
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
+
+        private static boolean second(char[][] arr, int i, int j, int count) {
+            int cntA =0;
+            int cntB =0;
+            int index =0;
+            for(int k = 2 ; k >= 0 ; k--){
+                int second = arr[index++][k] - '0';
+                if (second == i) cntA ++;
+                if (second == j) cntB ++;
+                if (cntA > 0 && cntB > 0 && cntB+ cntA ==3){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static boolean first(char[][] arr, int i, int j, int count) {
+            int cntA = 0;
+            int cntB = 0;
+            for(int k = 0 ; k < 3; k ++){
+                int second = arr[k][k] - '0';
+                if (second == i){
+                    cntA ++;
+                }
+                if (second == j){
+                    cntB++;
+                }
+            }
+            if (cntA > 0 && cntB>0 && cntB+ cntA == 3){
+                return true;
+            }
+            return false;
+        }
+
+        private static boolean height(char[][] arr, int i, int j, int count) {
+            //2.세로 가자
+            for(int l = 0 ; l < 3; l ++){
+                for(int z = 0 ; z < 3 ; z++){
+                    int check = arr[z][l] - '0';
+                    if (check == i || check == j){
+                        int cntA = 0;
+                        if (check == i){
+                            cntA ++;
+                        }
+                        int cntB = 0;
+                        if(check == j){
+                            cntB ++;
+                        }
+
+                        for (int index = z + 1; index < 3; index++){
+                            int second = arr[index][l] - '0';
+                            if (second == i){
+                                cntA ++;
+                            }
+                            if (second == j){
+                                cntB++;
+                            }
+                        }
+                        if (cntA > 0 && cntB > 0 && (cntA+cntB == 3)){
+                            return true;
+                        }
+                    }
+
+                }
+            }
+            return false;
+        }
+
+        private static boolean width(char[][] arr, int i, int j, int count) {
+            //1.가로부터 가자
+            for(int l = 0 ; l < 3; l ++){
+                for(int z = 0 ; z < 3 ; z++){
+                    int check = arr[l][z] - '0';
+                    if (check == i || check == j){
+                        int cntA = 0;
+                        if (check == i){
+                            cntA ++;
+                        }
+                        int cntB = 0;
+                        if(check == j){
+                            cntB ++;
+                        }
+
+                        for (int index = z + 1; index < 3; index++){
+                            int second = arr[l][index] - '0';
+                            if (second == i){
+                                cntA ++;
+                            }
+                            if (second == j){
+                                cntB++;
+                            }
+                        }
+                        if (cntA > 0 && cntB > 0 && (cntA+cntB == 3)){
+                            return true;
+                        }
+                    }
+
+                }
+            }
+            return false;
+        }
+    }
