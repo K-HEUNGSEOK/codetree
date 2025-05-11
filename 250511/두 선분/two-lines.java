ddbacfd3f30d701a74236db5b335da952f5cf827
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-11 17:42:27 +0900

    Title: 두 선분, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 250511/두 선분/two-lines.java
+++ 250511/두 선분/two-lines.java
@@ -5,27 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-    
+     
         Scanner sc = new Scanner(System.in);
         int x1 = sc.nextInt();
         int x2 = sc.nextInt();
         int x3 = sc.nextInt();
         int x4 = sc.nextInt();
 
-        int [] arr = new int[101];
-
-        for(int i = x1; i <= x2 ; i ++){
-          arr[i] ++;
-        }
-        for(int i = x3; i <= x4 ; i ++){
-            arr[i] ++;
-        }
-        boolean check = false;
-        for (int i : arr) {
-            if (i >= 2){
-                check = true;
-                break;
-            }
+      
+        boolean check = true;
+        if (x2 < x3 || x4 < x1){
+            check = false;
         }
         if (check){
             System.out.println("intersecting");
