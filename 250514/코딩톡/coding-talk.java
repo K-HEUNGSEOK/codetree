3235591266f715c099463b9ce491e7f2f58e3b6e
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-14 12:06:43 +0900

    Title: 코딩톡, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250514/코딩톡/coding-talk.java
@@ -0,0 +1,34 @@
+import javax.swing.table.TableCellRenderer;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+      
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int p = sc.nextInt()-1;
+
+        boolean[] person = new boolean[n];
+        char [] c = new char[m];
+        int [] u = new int[m];
+        for(int i = 0; i < m ; i++){
+            c[i] = sc.next().charAt(0);
+            u[i] = sc.nextInt();
+        }
+
+        for(int i = p ; i< m ; i ++){
+            person[c[i] - 'A'] = true;
+        }
+      
+        for(int i = 0 ; i < n ; i ++){
+            if (!person[i]){
+                int x = i + 'A';
+                System.out.print((char) x + " ");
+            }
+        }
+    }
+}
