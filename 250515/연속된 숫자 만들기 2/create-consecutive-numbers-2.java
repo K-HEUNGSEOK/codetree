58a9ab7d10046387dfbde85b1b8c50b26af42242
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-15 15:11:09 +0900

    Title: 연속된 숫자 만들기 2, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

--- 250515/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 250515/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-   
+       
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if (b == a + 1 && b == c -1 || b == a -1 && b == c + 1){
+        if (isCheck(a,b,c)){
             System.out.println(0);
         }else if(isCheck(b+1, b, c)){
             System.out.println(1);
