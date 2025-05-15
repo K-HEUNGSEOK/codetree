a7356e91bc73608ff26006f353b805943ea08702
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-15 14:43:35 +0900

    Title: 순 간 이 동, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

--- 250515/순 간 이 동/teleportation.java
+++ 250515/순 간 이 동/teleportation.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       
+    
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int x = sc.nextInt();
         int y = sc.nextInt();
 
-        int first = b - a;
-        int second = Math.abs(a - x) + (b - y); // x까지 가서 , y로 텔레포트하고 b로 가는거
-        int third = Math.abs(a - y) + (b - x);
+        int first = Math.abs(b - a);
+        int second = Math.abs(a - x) + Math.abs(b - y); // x까지 가서 , y로 텔레포트하고 b로 가는거
+        int third = Math.abs(a - y) + Math.abs(b - x);
         int ans = Math.min(first,second);
         ans = Math.min(ans,third);
         System.out.println(ans);
