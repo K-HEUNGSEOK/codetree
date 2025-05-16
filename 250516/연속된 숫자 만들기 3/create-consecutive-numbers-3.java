763d5a5f7d417bb3f59d9a785927a6eee757db02
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-16 21:51:03 +0900

    Title: 연속된 숫자 만들기 3, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 250516/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
+++ 250516/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -6,46 +6,11 @@ import java.util.Scanner;
 public class Main {
     static int n;
     public static void main(String[] args) throws IOException {
-       
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int cnt = 0;
-
-        while (true){
-            if (a == b-1 && c == b + 1){
-                System.out.println(cnt);
-                return;
-            }
-
-            if (isCheck(a,b,c)){
-                a = b;
-                b += 1;
-            }else if(isCheck2(a,b,c)){
-                c = b;
-                b -= 1;
-            }
-            cnt ++;
-        }
-
-
+       int a = sc.nextInt();
+int b = sc.nextInt();
+int c = sc.nextInt();
+int ans = Math.max(b - a, c - b) - 1;
+System.out.println(ans);
     }
-    private static boolean isCheck2(int a, int b, int c){
-        c = b -1;
-        if (a == c){
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean isCheck(int a, int b, int c) {
-        a = b + 1;
-        if (a == c){
-            return false;
-        }
-        return true;
-    }
-
-
 }
