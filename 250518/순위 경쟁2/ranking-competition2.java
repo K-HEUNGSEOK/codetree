f983847b3c6bfadbf95d67a8d3fc677396a66a33
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-18 01:30:51 +0900

    Title: 순위 경쟁2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250518/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,53 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cntA = 0; //A
+        int cntB = 0; //B
+        int ans = 0; //정답
+        int first = 0;
+        for(int i = 0 ; i < n ; i ++){
+        char c = sc.next().charAt(0);
+        int score = sc.nextInt();
+        if (c == 'A'){
+            cntA += score;
+        }else {
+            cntB += score;
+        }
+
+        if (cntA > cntB){
+            if (first != 2){
+                first = 2;
+                ans ++;
+            }
+        }else if(cntB > cntA){
+            if (first != 1){
+                first = 1;
+                ans ++;
+            }
+        }
+        else if(cntA == cntB){
+            if (first != 3){
+                first = 3;
+                ans ++;
+            }
+        }
+        
+        }
+        System.out.println(ans);
+
+
+
+    }
+
+//    static int[][] solution(int[][] a, int[][] b, int l, int r){
+//
+//    }
+}
