764ba7ce5ec869e250087e17d0fc4eb955c21ba1
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-19 14:46:49 +0900

    Title: L, R 그리고 B, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250519/L, R 그리고 B/l-r-and-b.java
+++ 250519/L, R 그리고 B/l-r-and-b.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       
+      
         Scanner sc = new Scanner(System.in);
         char[][] arr = new char[10][10];
         for(int i = 0 ; i < 10 ; i ++){
@@ -14,30 +14,36 @@ public class Main {
         }
         int lIdxI = 0;
         int lIdxR = 0;
+        int bIdxI = 0;
+        int bIdxR = 0;
+
+        int rIdxR = 0;
+        int rIdxL = 0;
         for(int i = 0 ; i < 10 ; i++){
             for(int j = 0 ; j < 10; j ++){
                 if (arr[i][j] == 'L'){
                     lIdxI = i;
                     lIdxR = j;
-                    break;
                 }
-            }
-        }
-
-        int bIdxI = 0;
-        int bIdxR = 0;
-        for(int i = 0 ; i < 10 ; i++){
-            for(int j = 0 ; j < 10; j ++){
                 if (arr[i][j] == 'B'){
                     bIdxI = i;
                     bIdxR = j;
-                    break;
+                }
+                if (arr[i][j] == 'R'){
+                    rIdxL = i;
+                    rIdxR = j;
                 }
             }
         }
 
-        int total = (Math.abs((lIdxI + lIdxR) - (bIdxI + bIdxR)));
+        int total = 0 ;
+        if (rIdxR == bIdxR && bIdxR == lIdxR){
+            total = (Math.abs((lIdxI + lIdxR) - (bIdxI + bIdxR)))+1;
+            System.out.println(total);
+        }else{
+            total = (Math.abs((lIdxI + lIdxR) - (bIdxI + bIdxR)));
         System.out.println(total-1);
+        }
     }
 
 //    static int solution(int[] arr, int[] copy , int n){
