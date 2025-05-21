27ff5da974d76df61e34054a8649a17ec2fc63ba
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-21 16:23:31 +0900

    Title: 최소 와이파이 수, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250521/최소 와이파이 수/minimum-number-of-wifi.java
+++ 250521/최소 와이파이 수/minimum-number-of-wifi.java
@@ -8,15 +8,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+     
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] arr = new int[n];
+        int cnt = 0;
         for(int i = 0 ; i < arr.length; i ++){
             arr[i] = sc.nextInt();
+            if (arr[i] == 1){
+                cnt ++;
+            }
         }
-        if (n % 2 == 0) System.out.println(n / (2 * m + 1));
+        if (m == 0){
+            System.out.println(cnt);
+        }
+        else if (n % 2 == 0) System.out.println(n / (2 * m + 1));
         else {
             System.out.println(n / (2 * m + 1) + 1);
         }
