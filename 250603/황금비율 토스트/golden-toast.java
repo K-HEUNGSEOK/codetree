bac0de0c848e74090c6349254fa3096c77b6e9f2
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-06-03 16:41:41 +0900

    Title: 황금비율 토스트, Time: 3120ms, Memory: 187MB, Status: Time Limit Exceed - Codetree

---
+++ 250603/황금비율 토스트/golden-toast.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+       
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int T = sc.nextInt();
+
+        List<Character> list = new LinkedList<>();
+        String str = sc.next();
+        sc.nextLine();
+        for(int i = 0 ; i < n ; i ++){
+            list.add(str.charAt(i));
+        }
+        ListIterator<Character> il = list.listIterator(list.size()); //만듬
+        for(int i = 0 ; i <T; i ++) {
+            String method = sc.nextLine();
+
+            switch (method){
+                case "L":
+                    if (il.hasPrevious()){
+                        il.previous();
+                    }
+                    break;
+                case "R":
+                    if (il.hasNext()){
+                        il.next();
+                    }
+                    break;
+                case "D":
+                    if (il.hasNext()){
+                        il.next();
+                        il.remove();
+                    }
+                    break;
+                default:
+                    String[] split = method.split(" ");
+                    String s = split[1];
+                    il.add(s.charAt(0));
+            }
+        }
+        for (Character c : list) {
+            System.out.print(c);
+        }
+    }
+}
