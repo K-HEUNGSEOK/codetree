439f8994a839e4943f4d1c6a6b080cb796697185
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-06-13 18:36:35 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 145ms, Memory: 10MB, Status: Passed - Codetree

--- 250613/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
+++ 250613/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
+   
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] x = new int[n];
@@ -27,6 +27,6 @@ public class Main {
         System.out.println(ans);
     }
     static int getNumber(int x1, int y1, int x2, int y2){
-        return Math.abs(x1 - x2) + Math.abs(y1 + y2);
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
 }
