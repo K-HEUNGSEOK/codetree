df921b7d1d5501d2f5fbe4ef11bfc8be18787c87
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-06-14 14:50:34 +0900

    Title: 오목, Time: 160ms, Memory: 11MB, Status: Wrong Answer - Codetree

--- 250614/오목/O-mok.java
+++ 250614/오목/O-mok.java
@@ -7,7 +7,7 @@ public class Main {
     static int[] dx = {-1,0,1,0};
     static int[] dy = {0,1,0,-1};
     public static void main(String[] args) throws IOException {
-       
+    
         Scanner sc = new Scanner(System.in);
         arr = new int[19][19];
         for (int i = 0; i < 19; i++) {
