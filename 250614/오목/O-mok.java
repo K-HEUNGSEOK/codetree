88fe49ff5d36edaced4536c01057108dc93ff942
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-06-14 14:54:23 +0900

    Title: 오목, Time: 150ms, Memory: 11MB, Status: Passed - Codetree

--- 250614/오목/O-mok.java
+++ 250614/오목/O-mok.java
@@ -1,49 +1,53 @@
-import java.io.*;
-import java.sql.SQLOutput;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int[][] arr;
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};
-    public static void main(String[] args) throws IOException {
+    public static final int DIR_NUM = 8;
     
+    public static int[][] arr = new int[19][19];
+    
+    public static int[] dx = new int[]{1, 1, 1, -1, -1, -1, 0, 0};
+    public static int[] dy = new int[]{-1, 0, 1, -1, 0, 1, -1, 1};
+    
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < 19 && 0 <= y && y < 19;
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        arr = new int[19][19];
-        for (int i = 0; i < 19; i++) {
-            for (int j = 0; j < 19; j++) {
+        // 입력
+        for(int i = 0; i < 19; i++)
+            for(int j = 0; j < 19; j++)
                 arr[i][j] = sc.nextInt();
-            }
-        }
-        for(int i = 0 ; i < 19; i ++){
-            for(int j = 0 ; j < 19 ; j ++){
-                if (arr[i][j] != 0){
-                    int current = arr[i][j];
+        
+        // 모든 좌표에서 다 확인해봅니다.
+        for(int i = 0; i < 19; i++) {
+            // 격자를 벗어나지 않을 범위로만 잡습니다.
+            for(int j = 0; j < 19; j++) {
+                if(arr[i][j] == 0) continue;
+                
+                for(int k = 0; k < DIR_NUM; k++) {
+                    int curt = 1;
                     int curx = i;
                     int cury = j;
-                    for(int k = 0 ; k < 4; k ++){
+                    while(true) {
                         int nx = curx + dx[k];
                         int ny = cury + dy[k];
-                        int cnt = 1;
-                        for(int l = 0 ; l < 4; l ++){
-                            if (!isRange(nx,ny))break;
-                            if (arr[nx][ny] != current) break;
-                            nx += dx[k];
-                            ny += dy[k];
-                            cnt ++;
-                        }
-                        if (cnt == 5){
-                            System.out.println(current);
-                            System.out.print((i + 2 * dx[k] + 1) + " " + (j + 2 * dy[k] + 1));
-                            System.exit(0);
-                        }
+                        if(inRange(nx, ny) == false)
+                            break;
+                        if(arr[nx][ny] != arr[i][j])
+                           break;
+                        curt++;
+                        curx = nx;
+                        cury = ny;
+                    }
+                    if(curt == 5) {
+                        System.out.println(arr[i][j]);
+                        System.out.print((i + 2 * dx[k] + 1) + " " + (j + 2 * dy[k] + 1));
+                        System.exit(0);
                     }
                 }
             }
         }
-
-    }
-    static boolean isRange(int nx, int ny){
-        return 0 <= nx && nx < 19 && 0 <= ny && ny < 19;
+        System.out.print(0);
     }
 }
