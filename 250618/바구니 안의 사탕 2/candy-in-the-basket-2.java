9a3efb33e6fbfde74ffeea6d3c8fccb3d6bf04ce
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-06-18 14:00:49 +0900

    Title: 바구니 안의 사탕 2, Time: 139ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250618/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int [] box = new int[101];
+        for(int i = 0; i < n ; i ++){
+            int can = sc.nextInt();
+            int p = sc.nextInt();
+            box[p] += can;
+        }
+        int total = 0;
+        if (n < k){
+            for (int i : box) {
+                total += i;
+            }
+            System.out.println(total);
+            System.exit(0);
+        }
+        int ans = 0;
+        for(int i = 1; i <= 200; i ++){
+            int sum = 0;
+            for(int j = i - k ; j <= i + k; j ++){
+                if (j < 1 || j > 100) break;
+                sum += box[j];
+            }
+            ans = Math.max(ans,sum);
+        }
+        System.out.println(ans);
+    }
+}
