33ab9a03d3abba874065f1ed8d4ea53adac56e35
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-06-20 14:05:29 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 115ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250620/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] x = new int[101];
+        int [] y = new int[101];
+        for(int i = 0 ; i <  n; i ++){
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+        int ans = 40000000;
+        for(int i = 0 ; i < n ; i ++){
+            int minx = Integer.MAX_VALUE;
+            int maxx = 0;
+            int miny = Integer.MAX_VALUE;
+            int maxy = 0;
+            for(int j = 0 ; j < n ; j  ++){
+                if (i==j)continue;
+
+                maxx = Math.max(maxx, x[j]);
+                minx = Math.min(minx,x[j]);
+                maxy = Math.max(maxy, y[j]);
+                miny = Math.min(miny,y[j]);
+            }
+            ans = Math.min(ans, (maxx-miny) * (maxy-miny));
+        }
+        System.out.println(ans);
+
+    }
+}
