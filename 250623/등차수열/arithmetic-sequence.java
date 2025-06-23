8232e952259e4c2fe2902cbc722aba99b6ec1fed
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-06-23 13:33:47 +0900

    Title: 등차수열, Time: 119ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250623/등차수열/arithmetic-sequence.java
+++ 250623/등차수열/arithmetic-sequence.java
@@ -8,23 +8,18 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-    
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n + 1];
         for(int i = 0 ; i < n ; i ++){
             arr[i] = sc.nextInt();
         }int ans = 0;
-        for(int i = 1 ; i <= 100; i ++){
-            for(int j = 0 ; j < n ; j ++){
-                int first = arr[j] - i;
-                for(int k = 0; k < n ;k ++){
-                    if (k==j) continue;
-                    if (1 <= arr[j] && arr[j] <= arr[k]){
-                    int second = i - arr[k];
-                    if (first == second){
-                        ans++;
-                    }
+        for(int k = 1 ; k <= 100; k ++){
+            for(int i = 0 ; i < n ; i ++){
+                for(int j = i + 1; j < n ; j ++){
+                    if (arr[i] - k == k - arr[j]){
+                        ans ++;
                     }
                 }
             }
