c58203454ef4d4f2e208c17cb7660b9c566c8103
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-06-24 18:05:32 +0900

    Title: 좌표평면 위의 균형 2, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

--- 250624/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.java
+++ 250624/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
     static final int MAX = 110;
     public static void main(String[] args) throws FileNotFoundException {
-       =
+       
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int [] x = new int[MAX];
