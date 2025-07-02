816841f48713a6f48235a2adfea518cf9e94b45e
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-07-02 14:57:26 +0900

    Title: 여러가지 진수변환, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250702/여러가지 진수변환/various-numeral-system-transformations.java
+++ 250702/여러가지 진수변환/various-numeral-system-transformations.java
@@ -6,14 +6,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-    
+     
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
        changeNumber(a,b);
     }
     static void changeNumber(int a, int b){
-        if (a > 1){
+        if (a / b != 0){
             changeNumber(a/b, b);
         }
         System.out.print(a % b);
