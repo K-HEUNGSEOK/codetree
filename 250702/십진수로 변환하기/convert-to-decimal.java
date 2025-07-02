5fe82de52efbc5380c31e5be403e4597e868f5d1
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-07-02 14:49:20 +0900

    Title: 십진수로 변환하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250702/십진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,22 @@
+import javax.print.DocFlavor;
+import java.io.*;
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+      \
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(digit(n));
+    }
+    static int digit(int n){
+        int num = 0;
+        char[] arr = String.valueOf(n).toCharArray();
+        for(int i = 0 ; i < arr.length; i ++){
+            num = num * 2 + (arr[i] -'0');
+        }
+        return num;
+    }
+}
