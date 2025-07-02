655c495e6223bd3ee40dfaeea641517575d50016
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-07-02 14:49:40 +0900

    Title: 십진수로 변환하기, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 250702/십진수로 변환하기/convert-to-decimal.java
+++ 250702/십진수로 변환하기/convert-to-decimal.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-      \
+    
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(digit(n));
