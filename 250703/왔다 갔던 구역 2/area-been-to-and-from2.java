4e63755427881419ab08e43e310fa108a44bf96b
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-07-03 14:05:37 +0900

    Title: 왔다 갔던 구역 2, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 250703/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250703/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -6,52 +6,39 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-      
+    
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int [] simulation = new int[201];
-        int offset = 100;
         int current = 100;
+        int[] x1 = new int[201];
+        int[] x2 = new int[201];
         for(int i = 0 ; i < n ; i ++){
-            int d  = sc.nextInt();
+            int way = sc.nextInt();
             char command = sc.next().charAt(0);
 
             if (command == 'L'){
-                int distance = current - d;
-                for(int j = current; j >= distance; j --){
-                    simulation[j] ++;
-                }
-                current -= d;
+                x1[i] = current - way;
+                x2[i] = current;
+                current -= way;
             }else{
-                int distance = d + current;
-                for(int j = current; j <= distance; j ++){
-                    simulation[j] ++;
-                }
-                current += d;
+                x1[i] = current;
+                x2[i] = current + way;
+                current += way;
             }
         }
-        int lt = 0, rt = 0, ans = 0;
-        int len = 201;
 
-        while (true) {
-            // 1) ≤1인 정점 건너뛰기
-            while (rt < len && simulation[rt] <= 1) {
-                rt++;
+        for(int i = 0 ; i < n ; i ++){
+            for(int j = x1[i] ; j < x2[i] ; j ++){
+                simulation[j] ++;
             }
-            if (rt >= len) break;      // 더 이상 >1인 정점이 없으면 종료
-
-            // 2) >1인 정점 구간 시작
-            lt = rt;
-
-            // 3) 이어지는 >1인 정점 구간 찾기
-            while (rt < len && simulation[rt] > 1) {
-                rt++;
-            }
-
-            // 4) 구간 길이(rt−lt)에서 1을 빼서 “간선 개수”로 환산
-            ans += (rt - lt - 1);
         }
-
-        System.out.println(ans);
+        int cnt = 0;
+        for(int i = 0 ; i < 201; i ++){
+            if (simulation[i] >= 2){
+                cnt ++;
+            }
+        }
+        System.out.println(cnt);
     }
 }
