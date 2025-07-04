f3d1e48fc539fcfc3446bc3ff1579bf927fc0469
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-07-04 22:38:37 +0900

    Title: 흰검 칠하기, Time: 136ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250704/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,54 @@
+import java.io.*;
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+      
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] white = new int[10001];
+        int[] black = new int[10001];
+        int[] box = new int[10001];
+        int current = 1000;
+        for(int i = 0; i < n ; i ++){
+            int way = sc.nextInt();
+            char command = sc.next().charAt(0);
+
+            if (command == 'R'){
+                int d = way + current;
+                for(int m = current; m < d; m ++){
+                    black[m] ++;
+                    box[m] = 2;
+                }
+                current += way -1;
+            }else{
+                int d = current - way + 1;
+                for(int m = current; m >= d; m --){
+                    white[m]++;
+                    box[m]= 1;
+                }
+                current = current - way + 1;
+            }
+        }
+        int w = 0;
+        int b = 0;
+        int g = 0;
+        for(int i = 0 ; i < 10001; i ++){
+            if (box[i] > 0 && white[i] >= 2 && black[i] >= 2 ){
+                box[i] = 3;
+            }
+        }
+        for(int i = 0 ; i < 10001; i ++){
+            if (box[i] == 1){
+                w ++;
+            }else if(box[i] == 2){
+                b++;
+            }else if(box[i] == 3){
+                g ++;
+            }
+        }
+        System.out.println(w + " " +  b + " " + g);
+    }
+}
