8f63cb26035f1034bd2d66d197d5292969469301
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-08-03 23:19:07 +0900

    Title: 최고의 33위치, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250803/최고의 33위치/best-place-of-33.java
+++ 250803/최고의 33위치/best-place-of-33.java
@@ -7,35 +7,37 @@ import java.util.*;
 
 public class Main {
     static int n;
+    static int[][] arr;
     public static void main(java.lang.String[] args) throws IOException {
-      
+ 
         Scanner sc = new Scanner(System.in);
         int ans = 0;
         n = sc.nextInt();
-        int[][] arr = new int[n][n];
+        arr = new int[n][n];
         for(int i = 0 ; i < n ; i ++){
             for(int j = 0 ; j < n ; j ++){
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        for(int i = 0 ; i < n ; i ++){
-            for(int j = 0 ; j < n ; j ++){
-                int cnt = 0 ; // 여기서 최대 갯수 세기
-                for(int k = i ; k < 3 + i ; k ++){
-                    for(int l = j ; l < 3 + j; l ++){
-                        if (k >= n || l >= n) continue;
-                        if (arr[k][l] == 1){
-                            cnt ++;
-                        }
-                    }
-                }
-                ans = Math.max(ans, cnt);
+        for(int i = 0 ; i < n -2; i ++){
+            for(int j = 0 ; j < n -2; j ++){
+                int number = getMoney(i,j, i +2 , j + 2);
+
+                ans = Math.max(ans, number);
             }
         }
         System.out.println(ans);
     }
-    static boolean isRange(int x, int y){
-        return 0 <= x && x < n && 0 <= y && y < n;
+    static int getMoney(int rows, int cols , int rowe, int cole){
+        int cnt = 0;
+        for(int i = rows ; i <= rowe; i ++){
+            for(int j = cols; j <= cole ; j++){
+                if (arr[i][j] == 1){
+                    cnt ++;
+                }
+            }
+        }
+        return cnt;
     }
 }
