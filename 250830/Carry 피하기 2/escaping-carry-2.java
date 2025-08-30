53b6640ebac56f0f2789651e54ae16bdc989f4a3
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-08-30 10:39:58 +0900

    Title: Carry 피하기 2, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 250830/Carry 피하기 2/escaping-carry-2.java
+++ 250830/Carry 피하기 2/escaping-carry-2.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-    
+     
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n ; i ++){
             arr[i] = sc.nextInt();
         }
-        int ans = 0;
+        int ans = -1;
 
         for(int i = 0 ; i < n ; i ++){
             for(int j = 0 ; j < n ; j ++){
